8471009e441c99a398526e47961b7787cfcf19f8
abhinavmabhi <dexmarketing192018@gmail.com>
2025-12-16 16:03:43 +0530

    commit

--- kingkarakk/urls.py
+++ kingkarakk/urls.py
@@ -2,10 +2,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 from app import views
-from django.contrib import admin
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
 
 
     # ================== ADMIN  ==================
